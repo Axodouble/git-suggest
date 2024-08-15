69f6c5b4736ccbb80aeefef68b799bc44993ed4d
Axodouble <git@axodouble.com>
2024-08-15 09:24:48 +0000

    Made suggestions modal based & added readme.

--- index.js
+++ index.js
@@ -1,48 +1,72 @@
-const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
-const { Octokit } = require("@octokit/rest");
-require("dotenv").config();
+const { Client, GatewayIntentBits, SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-client.once('ready', () => {
+client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.application.
 
-    client.application.commands.create(new SlashCommandBuilder()
-        .setName('suggest')
-        .setDescription('Suggest a feature or report a bug')
-        .addStringOption(option =>
-            option.setName('title')
-                .setDescription('Title of the GitHub issue')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('description')
-                .setDescription('Detailed description of the issue')
-                .setRequired(true))
-    )
+        client.application.commands.create(new SlashCommandBuilder()
+            .setName('suggest')
+            .setDescription('Suggest a feature')
+        );
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) {
+        if (interaction.commandName === 'suggest') {
+            showSuggestionsModal(interaction);
+        }
+    }
+    if (interaction.isModalSubmit()) {
+        if (interaction.customId === 'suggestion') {
+            try {
+                const { Octokit } = await import('@octokit/rest');
+                const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+                const { data } = await octokit.issues.create({
+                    owner: process.env.GITHUB_OWNER,
+                    repo: process.env.GITHUB_REPO,
+                    body: interaction.fields.getTextInputValue('description'),
+                    title: interaction.fields.getTextInputValue('title'),
+                    labels: ['suggestion']
+                });
 
-    const { commandName } = interaction;
-
-    if (commandName === 'suggest') {
-        try {
-            // Create a GitHub issue
-            const { data } = await octokit.issues.create({
-                owner: process.env.GITHUB_OWNER,
-                repo: process.env.GITHUB_REPO,
-                title: interaction.options.getString('title'),
-                body: interaction.options.getString('description'),
-            });
-
-            await interaction.reply(`GitHub issue created successfully: ${data.html_url}`);
-        } catch (error) {
-            console.error(error.stack);
-            await interaction.reply('There was an error creating the GitHub issue.');
+                await interaction.reply(`Suggestion created succesfully: ${data.html_url}`);
+            } catch (error) {
+                console.error(error.stack);
+                await interaction.reply('There was an error creating the GitHub issue.');
+            }
         }
     }
 });
+
+function showSuggestionsModal(interaction) {
+    interaction.showModal(
+        new ModalBuilder()
+            .setTitle("Suggestions")
+            .setCustomId("suggestion")
+            .setComponents([
+                new ActionRowBuilder().setComponents([
+                    new TextInputBuilder()
+                        .setLabel("Title")
+                        .setPlaceholder("Enter the title of the suggestion")
+                        .setCustomId("title")
+                        .setRequired(true)
+                        .setStyle(TextInputStyle.Short),
+                ]),
+                new ActionRowBuilder().setComponents([
+                    new TextInputBuilder()
+                        .setLabel("Description")
+                        .setPlaceholder("Enter the description of the suggestion")
+                        .setCustomId("description")
+                        .setRequired(true)
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setMinLength(10)
+                        .setMaxLength(2000),
+                ]),
+            ])
+    )
+}
 
 client.login(process.env.DISCORD_TOKEN);
---
+++ readme.md
@@ -0,0 +1,11 @@
+# discord to github suggestion synchroniser
+
+this allows discord users to create an issue / suggestion on a github repo with a command within a discord guild.
+
+to-do:
+
+- [ ] make all suggestions / issues panel-based
+- [ ] dynamically allocate buttons on the panel to a single config file
+- [ ] support for voting / approving suggestions before they are sent to the github
+- [ ] support for blacklist role for suggestions
+- [ ] support for suggestion log channel
